25b7a574530a74650274d6713cdee89e22e32436
Khushboo Goel <30549153+khushboo-goel@users.noreply.github.com>
2023-09-10 18:33:26 +0530

    Update reverse.cpp

--- Maths/reverse.cpp
+++ Maths/reverse.cpp
@@ -2,17 +2,17 @@
 // NOT the best approach since the question says environment doesnt support 64-bit integer and we used long long.
 
 int reverse(int x) {
-    long long reverse = 0;
-    int number = x;
-
-    while(number != 0) {
-        int temp = (number) % 10;
-        reverse = (reverse * 10) + temp;
-        if (reverse > INT_MAX || reverse < INT_MIN) {
-            return 0;
-        }
-        number = number / 10;
+  int reverse = 0;
+  int number = x;
+  
+  while(number != 0) {
+    int temp = (number) % 10;
+    if (reverse > INT_MAX / 10 || reverse < INT_MIN / 10) {
+      return 0;
     }
-
-    return (int)reverse;
+    reverse = (reverse * 10) + temp;
+    number = number / 10;
+  }
+  
+  return (int)reverse;
 }
