6242f334bc9834e258ea725756d7ce9ffdf8bdd1
Khushboo Goel <30549153+khushboo-goel@users.noreply.github.com>
2023-09-11 11:55:23 +0530

    Create readme.cpp for BS

---
+++ BinarySearch/readme.cpp
@@ -0,0 +1,6 @@
+# Binary Search
+
+## Type of Questions to cover:
+1. Basic Binary search like finding element in an array.
+2. Hard problems like searching median of two sorted arrays.
+3. Questions where you need to find a search space (not the array dierctly) and use binary search in the search space.
