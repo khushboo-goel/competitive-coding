0099b3e15bf9bc8dfef473d0eff61322e86a0f9b
Khushboo Goel <30549153+khushboo-goel@users.noreply.github.com>
2023-10-15 20:26:48 +0530

    Create longestContinousSequence.cpp

---
+++ Arrays/longestContinousSequence.cpp
@@ -0,0 +1,41 @@
+// https://leetcode.com/problems/longest-consecutive-sequence/description/
+int longestConsecutive(vector<int>& nums) {
+    int len = nums.size();
+    map<int, int> freq;
+    if (len == 0 || len == 1) {
+        return len;
+    }
+
+    for (int i = 0; i < len; i++) {
+        if (freq.find(nums[i]) == freq.end()) {
+            freq[nums[i]] = 1;
+        } else {
+            freq[nums[i]] += 1;
+        }
+    }
+
+    // create list of uniques to optimise the function
+    vector<int> uniques(1, nums[0]);
+    for (int i = 1; i < len; i++) {
+        if (nums[i] != nums[i - 1]) {
+            uniques.push_back(nums[i]);
+        }
+    }
+
+    int maxLen = 1;
+    for (int i = 0; i < uniques.size(); i++) {
+        if (freq.find(uniques[i] - 1) == freq.end()) {
+            int num = uniques[i];
+            int currLen = 0;
+
+            while(freq.find(num) != freq.end()) {
+                num++;
+                currLen++;
+            }
+
+            maxLen = max(maxLen, currLen);
+        }
+    }
+
+    return maxLen;
+}
